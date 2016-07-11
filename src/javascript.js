8df0454d91dcf1a3d8085c1abfc8bb803bb4d4a1
Rubberkat <roydejager@live.nl>
2016-07-11 17:41:26 +0200

    added accordion animation

--- src/javascript.js
+++ src/javascript.js
@@ -2,7 +2,10 @@ $(document).ready(function () {
     var content = $(".dropdown-content" ).hide();
 
     var title = $(".dropdown-title").click(function () {
+        title.not(this).removeClass('active');
+        $(this).toggleClass("active");
         content.not($(this).next()).slideUp();
         $(this).next().slideToggle();
+
     });
 });
